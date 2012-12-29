92c34705e14ca0c6332f776a04712555534e81ac
hepa <hepa@hbox.(none)>
2012-12-28 23:34:21 -0500

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pulse::Application.config.secret_token = 'ab5c27c5d91db41acc652ed7fc79c5ce270304399b4e6434fb8f1063311febd41ae051c4f6c53cf02e0fecfeb8328195cece4a818722706e8254816d6a7ca778'
